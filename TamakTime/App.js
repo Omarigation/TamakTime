c7cce78548934fc29fe8173766259eb3624d7ff7
Omar Akim <omarakim2005@gmail.com>
2025-04-13 01:37:15 +0500

    init

---
+++ TamakTime/App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, SafeAreaView, Button, Alert, Image, TouchableHighlight, View } from 'react-native';
+
+export default function App() {
+
+  const dd = () => Alert.alert('Tamak Times', 'UseFull web application', [
+    {text: 'Yes', onPress: () => console.log('Yes')}, {text: 'No'}
+  ]);
+  return (
+    <View style = {styles.mainBlock}>
+      <View style={[styles.box, {backgroundColor: 'red', flex: 6}]}></View>
+      <View style={[styles.box, {backgroundColor: 'blue', flex: 2}]}></View>
+      <View style={[styles.box, {backgroundColor: 'black', flex: 2}]}></View>
+    </View>
+    // <SafeAreaView style={styles.container}>
+    //   <Text>Omar!!!</Text>
+    //   <Button title='Press' onPress={() => Alert.alert('Tamak Times', 'UseFull web application', [
+    //     {text: 'Yes', onPress: () => console.log('Yes')}, {text: 'No'}
+    //   ])}/>
+
+    //   <TouchableHighlight onPress={dd}>
+    //   <Image source={{
+    //     width: 200, height: 150, uri: "https://i.pinimg.com/736x/5e/7b/e0/5e7be05de1eb00ff2fb2246e843c17fb.jpg"
+    //   }
+        
+    //   }/>
+    //   </TouchableHighlight>
+    //   <StatusBar style="auto" />
+    // </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainBlock:{
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+  box: {
+    flex: 1,
+    backgroundColor: 'blue'
+  }
+});
